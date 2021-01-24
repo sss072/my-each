72ff6de513a5e9b7f69a76b565071b9df90f93d5
Sami <samsabeeh6@gmail.com>
2021-01-24 12:54:23 -0500

    commit tree 14cff6caef521918e2559988f268a48287f5ede8

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  i
+end 
